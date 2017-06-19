65807c3423eee5349add5e4f463e0f6d45973deb
Belen Curcio <curciobelen@gmail.com>
2017-06-18 23:11:22 -0300

    Initial Commit

---
+++ server.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// Template Caching
+var templates = template.Must(template.ParseFiles(
+	"templates/edit.html",
+	"templates/view.html",
+))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Post) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// Data Structures
+type Post struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Post) save() error {
+	filename := "data/" + p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPost(title string) (*Post, error) {
+	filename := "data/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Post{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	// Load Post
+	p, err := loadPost(title)
+
+	// Not Found, let's go to edit
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+
+	// Render View
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	// Load Post
+	p, err := loadPost(title)
+
+	// Not Found, let's create the post
+	if err != nil {
+		p = &Post{Title: title}
+	}
+
+	// Render Edit
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	// FormValue
+	body := r.FormValue("body")
+
+	// Creating and Saving the Post
+	p := &Post{Title: title, Body: []byte(body)}
+	err := p.save()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+// Error Handling
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func main() {
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	fmt.Println("Server is listening at port 8080")
+	http.ListenAndServe(":8080", nil)
+}
